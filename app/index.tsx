b783c137955ef4f48d07c2dfa7269f9efbfb649f
Joseph Ogbu <fallyfox@gmail.com>
2025-07-31 18:05:34 +0100

    designed intro screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,62 +1,28 @@
-import { customStyles } from "@/components/custom-styles";
-import { Image, StyleSheet, Text, View } from "react-native";
+import { introStyles } from "@/styles/intro.styles";
+import { ImageBackground, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
   return (
-    <View style={customStyles.main}>
-      <View>
-        <Text style={{
-          fontWeight: "bold",
-          color: "gray",
-          fontSize:24
-          }}>React Native Training</Text>
-      </View>
+    <View style={introStyles.cover}>
+      <ImageBackground
+      style={introStyles.bg}
+      source={require("../assets/images/intro_bg.jpg")}>
+        <View style={introStyles.layer}>
+          <View style={introStyles.header}>
+            <Text style={introStyles.title}>UniPeers</Text>
+          </View>
 
-      <View style={{
-        width: "100%",
-        minHeight: 160,
-        backgroundColor: "black"
-      }}>
-        <Text style={styles.text}>Course Banner</Text>
-        <Image
-        width={140}
-        height={280}
-        style={{
-          width:"100%"
-        }}
-        source={{uri: "https://plus.unsplash.com/premium_photo-1748087734665-fc0f73d83858?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}}
-        alt="course photo"/>
-      </View>
+          <View style={introStyles.body}>
+            <Text style={introStyles.bodyText}>The school app for socialising. Connect with friends, create your own event, and find other amazing events from students.</Text>
+          </View>
 
-      <View style={{
-        width: "100%",
-        minHeight: 160,
-        backgroundColor: "orange",
-        marginVertical: 16,
-        borderWidth: 4,
-        borderColor: "black",
-        borderRadius: 12
-      }}>
-        <Text style={styles.text}>About Me</Text>
-      </View>
-
-       <View style={{
-        width: "100%",
-        minHeight: 160,
-        backgroundColor: "purple"
-      }}>
-        <Text style={styles.text}>About Early Code Institute</Text>
-        <Image
-        source={require("../assets/images/react-logo.png")}
-        alt="logo"/>
-      </View>
+          <View>
+            <TouchableOpacity style={introStyles.btn}>
+              <Text style={introStyles.btnText}>Get Started</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ImageBackground>
     </View>
   )
 }
-
-const styles = StyleSheet.create({
-  text: {
-    fontSize: 16,
-    color: "white"
-  }
-});
