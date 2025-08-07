bd5df91802cc8a94a58f394af6efdd2538b7de90
Joseph Ogbu <fallyfox@gmail.com>
2025-08-07 18:14:46 +0100

    nativewind installation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,34 @@
-import { Text, View } from "react-native";
+import { events } from "@/assets/local-data/events";
+import { FlatList, Image, Text, View } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+
+function Seperator () {
+  return (
+    <View style={{height:16, backgroundColor:"transparent"}}></View>
+  )
+}
 
 export default function Index () {
   return (
-    <View style={{flex:1,justifyContent:"center",alignItems:"center"}}>
-      <Text>Home screen on tabs</Text>
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={{
+        flex: 1
+      }}>
+        <FlatList
+        data={events}
+        renderItem={({item}) => {
+          return (
+            <View>
+              <Image
+              source={{uri: item.bannerUrl}}
+              alt="event photo"
+              style={{width:600,height:400,resizeMode:"cover"}}/>
+              <Text className="text-sx font-bold">{item.title}</Text>
+            </View>
+          )
+        }}
+        keyExtractor={(item) => item.id}/>
+      </SafeAreaView>
+    </SafeAreaProvider>
   )
 }
