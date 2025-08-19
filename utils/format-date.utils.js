4e5aecf25d295916759a56c8f59482e2387e10ae
Joseph Ogbu <fallyfox@gmail.com>
2025-08-19 16:50:03 +0100

    datepicker and minor ui update

---
+++ utils/format-date.utils.js
@@ -0,0 +1,8 @@
+export const formatTimestampToDate = (date) => {
+    const day = date.toLocaleDateString("us",{ day: "numeric"});
+    const month = date.toLocaleDateString("us",{ month: "short"});
+    const year = date.getFullYear();
+    const formattedDate = `${day} ${month} ${year}`;
+
+    return formattedDate;
+}
