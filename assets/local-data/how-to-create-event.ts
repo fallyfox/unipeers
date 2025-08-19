3a9b115a898447c4330c486a522960f74f3b6c6a
Joseph Ogbu <fallyfox@gmail.com>
2025-08-19 10:26:09 +0100

    Platform, StatusBar, and TextInput

---
+++ assets/local-data/how-to-create-event.ts
@@ -0,0 +1,22 @@
+export const howToCreateEvent = [
+    {
+        title:"Title",
+        doc: "Choose a clear and catchy name for your event. What is it, and what's it about?"
+    },
+    {
+        title:"Price",
+        doc: "Decide if your event will be free or paid. If it's a paid event, enter the ticket price."
+    },
+    {
+        title:"Venue",
+        doc: "Where is your event happening? You can search for a location or manually enter an address."
+    },
+    {
+        title:"Time",
+        doc: "Set the date and time. Use the calendar to select the start and end times for your event."
+    },
+    {
+        title:"Description",
+        doc: "This is your chance to shine! Write a compelling description that gives people a reason to attend. Include all the important details they need to know."
+    },
+];
