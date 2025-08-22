0a2c8093ca1fe7176c710d02f60ceb43f798fb65
Joseph Ogbu <fallyfox@gmail.com>
2025-08-22 03:49:37 +0100

    react native select picker fix

--- app/(tabs)/create.jsx
+++ app/(tabs)/create.jsx
@@ -1,15 +1,30 @@
 import { howToCreateEvent } from "@/assets/local-data/how-to-create-event";
+import { schools } from "@/assets/local-data/school-list";
 import { formatTimestampToDate } from "@/utils/format-date.utils";
 import { themeColors } from "@/utils/theme.utils";
 import DateTimePicker from "@react-native-community/datetimepicker";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Platform, ScrollView, StatusBar, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import RNPickerSelect from 'react-native-picker-select';
 
 export default function Create () {
     const [title,setTitle] = useState("");
     const [description,setDescription] = useState("");
+    const [venue,setVenue] = useState("");
+    const [schoolOptions,setSchoolOptions] = useState([]);
+    const [selectedSchool,setSelectedSchool] = useState(null);
     const [date,setDate] = useState(new Date());
     const [showPicker,setShowPicker] = useState(false);
+
+    // make a simple list of schools
+    useEffect(() => {
+        const list = [];
+        schools.forEach(item => list.push({
+            label: item.title,
+            value: item.symbol
+        }));
+        setSchoolOptions(list);
+    },[]);
 
     const onChange = (event,selectedDate) => {
         const currentDate = selectedDate || date;
@@ -37,7 +52,7 @@ export default function Create () {
                         <Text className="text-md text-neutral-500">Event description</Text>
                         <TextInput
                         multiline={true}
-                        style={styles.textarea}
+                        style={styles.input}
                         placeholder="title of your event"
                         value={description}
                         onChangeText={(text) => setDescription(text)}/>
@@ -46,12 +61,7 @@ export default function Create () {
                     <View>
                         <TouchableOpacity 
                         onPress={() => setShowPicker(true)}
-                        style={{
-                            backgroundColor: themeColors.darkGray,
-                            borderRadius: 50,
-                            paddingHorizontal: 16,
-                            paddingVertical: 8
-                        }}
+                        style={styles.picker}
                         className="flex flex-row justify-between items-center">
                             <Text className="font-bold text-lg text-neutral-100">{formatTimestampToDate(date)}</Text>
                             <Text className="font-bold text-2xl text-neutral-100">Select event date</Text>
@@ -64,6 +74,24 @@ export default function Create () {
                             onChange={onChange}/>
                         )}
                     </View>
+
+                    <View>
+                        <Text className="text-md text-neutral-500">Event venue</Text>
+                        <TextInput
+                        style={styles.input}
+                        placeholder="exact event venue"
+                        value={venue}
+                        onChangeText={(text) => setVenue(text)}/>
+                    </View>
+                    
+                    {schoolOptions.length > 0 &&
+                    <View style={styles.input}>
+                        <Text>Choose school where event will be held</Text>
+                        <RNPickerSelect
+                        items={schoolOptions} // requires an object with label an value
+                        onValueChange={(item) => setSelectedSchool(item)}
+                        value={selectedSchool}/>
+                    </View>}
                 </View>
 
                 {/* how to create event - documentation */}
@@ -89,18 +117,16 @@ const styles = StyleSheet.create({
     },
     input: {
         borderWidth: 1,
-        borderColor: themeColors.darkGray,
-        paddingHorizontal: 16,
-        paddingVertical: 8,
-        borderRadius: 50,
-        fontSize: 16,
-    },
-    textarea: {
-        borderWidth: 1,
-        borderColor: themeColors.darkGray,
+        borderColor: themeColors.gray300,
         paddingHorizontal: 16,
         paddingVertical: 8,
         borderRadius: 8,
         fontSize: 16,
     },
+    picker: {
+        backgroundColor: themeColors.darkGray,
+        borderRadius: 8,
+        paddingHorizontal: 16,
+        paddingVertical: 8
+    }
 })
